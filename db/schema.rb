e0741d876922d70d69b716ecfd81e7fae00dcc8d
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-02-07 11:01:44 +0000

    Basics done. books model, controller and main views up

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_07_104302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.date "date"
+    t.string "identifier"
+    t.string "PublisherName"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
